c90e10c5150a059268debd6de6930b318bb9c874
Yosra H. Hekal <yu@Yosras-MacBook-Pro.local>
2018-07-12 17:58:41 +0200

    menu slider integrated, gradient view(reusable)

---
+++ Smack/Smack/View/GradientView.swift
@@ -0,0 +1,39 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Yosra H. Hekal on 12.07.18.
+//  Copyright © 2018 Yosra H. Hekal. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.3631127477, green: 0.4045833051, blue: 0.8775706887, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+        
+    }
+
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+
+}
