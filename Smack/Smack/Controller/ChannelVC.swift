b26cfd50137e03f6e3068343a40b426d9be324cf
Yosra H. Hekal <yu@Yosras-MacBook-Pro.local>
2018-07-12 02:47:32 +0200

    installing the library and get the menu slider to work

---
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Yosra H. Hekal on 12.07.18.
+//  Copyright © 2018 Yosra H. Hekal. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+}
