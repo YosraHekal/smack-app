ae74ab2ab6aa70b434f39da93fd10bf00a512d5d
Yosra H. Hekal <yu@Yosras-MacBook-Pro.local>
2018-08-02 16:17:30 +0200

    showiwng channcels, configureCell function, hover function(in view channel cell), and cellForRowAt, numberOfSections, numberofRowsinSection in channcelVC ..

---
+++ Smack/Smack/Model/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Yosra H. Hekal on 31.07.18.
+//  Copyright © 2018 Yosra H. Hekal. All rights reserved.
+//
+
+import Foundation
+
+struct Channel : Decodable {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+    
+    
+}
